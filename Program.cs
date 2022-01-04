33540913e2d8f2b480a79ac003663b45c53b9c3b
Carlos Lopez <carlos@wecancodeit.org>
2022-01-04 13:01:01 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace template_csharp_virtual_pet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
